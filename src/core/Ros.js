4c096ee141f3c2ccae3b7447c69fc5f3c7933623
garaemon <garaemon@gmail.com>
2013-10-16 09:08:40 -0700

    not defining recursive helper function in global scope

--- src/core/Ros.js
+++ src/core/Ros.js
@@ -319,52 +319,49 @@ ROSLIB.Ros.prototype.getMessageDetails = function(message, callback) {
 ROSLIB.Ros.decodeTypeDefs = function(type_defs) {
   var type_def_dict = {};
   var the_type = type_defs[0];
-  return ROSLIB.Ros._decodeTypeDefs(type_defs[0], type_defs);
-};
-
-/**
- * Internal function of ROSLIB.Ros.decodeTypeDefs.
- * It calls itself recursively to resolve type definition
- * using hint_defs.
- * @param the_type - array of type_def dictionary
- * @param hint_defs - array of typedefs
- */
-ROSLIB.Ros._decodeTypeDefs = function(the_type, hint_defs) {
-  var type_def_dict = {};
-  for (var i = 0; i < the_type.fieldnames.length; i++) {
-    var array_len = the_type.fieldarraylen[i];
-    var field_name = the_type.fieldnames[i];
-    var field_type = the_type.fieldtypes[i];
-    if (field_type.indexOf("/") === -1) { // check the field_type includes "/" or not
-      if (array_len == -1) {
-        type_def_dict[field_name] = field_type;
-      }
-      else {
-        type_def_dict[field_name] = [field_type];
-      }
-    }
-    else {
-      // lookup the name
-      var sub_type = false;
-      for (var j = 0; j < hint_defs.length; j++) {
-        if (hint_defs[j].type == field_type) {
-          sub_type = hint_defs[j];
-          break;
-        }
-      }
-      if (sub_type) {
-        var sub_type_result = ROSLIB.Ros._decodeTypeDefs(sub_type, hint_defs);
+  
+  // It calls itself recursively to resolve type definition
+  // using hint_defs.
+  var decodeTypeDefsRec = function(the_type, hint_defs) {
+    var type_def_dict = {};
+    for (var i = 0; i < the_type.fieldnames.length; i++) {
+      var array_len = the_type.fieldarraylen[i];
+      var field_name = the_type.fieldnames[i];
+      var field_type = the_type.fieldtypes[i];
+      if (field_type.indexOf("/") === -1) { // check the field_type includes "/" or not
         if (array_len == -1) {
-          type_def_dict[field_name] = sub_type_result;
+          type_def_dict[field_name] = field_type;
         }
         else {
-          type_def_dict[field_name] = [sub_type_result];
+          type_def_dict[field_name] = [field_type];
         }
       }
       else {
-        throw "cannot find " + field_type;
+        // lookup the name
+        var sub_type = false;
+        for (var j = 0; j < hint_defs.length; j++) {
+          if (hint_defs[j].type == field_type) {
+            sub_type = hint_defs[j];
+            break;
+          }
+        }
+        if (sub_type) {
+          var sub_type_result = decodeTypeDefsRec(sub_type, hint_defs);
+          if (array_len == -1) {
+            type_def_dict[field_name] = sub_type_result;
+          }
+          else {
+            type_def_dict[field_name] = [sub_type_result];
+          }
+        }
+        else {
+          throw "cannot find " + field_type;
+        }
       }
     }
-  }
-  return type_def_dict;
+    return type_def_dict;
+  };                            // end of decodeTypeDefsRec
+  
+  return decodeTypeDefsRec(type_defs[0], type_defs);
 };
+
