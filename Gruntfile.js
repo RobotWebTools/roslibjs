248505e19ed176a2ad18960898fcbb70fe6e6a5e
Florian Traverse <florian.traverse@gmail.com>
2014-04-02 16:07:20 +0200

    Integrate NodeJS+browser builds & use grunt through NPM

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,26 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       build: {
-        src  : ['src/RosLibHeader.js',
-                'src/RosLib.js',
-                'src/actionlib/*.js',
-                'src/core/*.js',
-                'src/math/*.js',
-                'src/tf/*.js',
-                'src/urdf/*.js',
-                'src/RosLibFooter.js'
-               ],
-        dest : 'build/roslib.js'
+        files: {
+          'build/roslib.node.js'  : [
+                  'src/RosLibHeader.js',
+                  'src/RosLib.js',
+                  'src/actionlib/*.js',
+                  'src/core/*.js',
+                  'src/math/*.js',
+                  'src/tf/*.js',
+                  'src/urdf/*.js',
+                  'src/RosLibFooter.js'
+                 ],
+          'build/roslib.js' : [
+                  'src/RosLib.js',
+                  'src/actionlib/*.js',
+                  'src/core/*.js',
+                  'src/math/*.js',
+                  'src/tf/*.js',
+                  'src/urdf/*.js'
+                 ]
+        }
       }
     },
     jshint: {
@@ -22,17 +32,31 @@ module.exports = function(grunt) {
       },
       files: [
         'Gruntfile.js',
-        'build/roslib.js'
+        'build/roslib.node.js',
+        'build/roslib.js',
       ]
     },
-    /*
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          require: [
+            function(){
+              chai = require('chai')
+              EventEmitter2 = require('eventemitter2').EventEmitter2
+              ROSLIB = require('./build/roslib.node.js')
+            }]
+        },
+        src: ['test/**/*.test.js']
+      }
+    },
     karma: {
       build: {
         configFile: 'test/karma.conf.js',
         singleRun: true,
         browsers: ['PhantomJS']
       }
-    },*/
+    },
     watch: {
       dev: {
         options: {
@@ -81,11 +105,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-jsdoc');
-//  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('dev', ['concat', 'watch']);
   grunt.registerTask('build', ['concat', 'jshint']); //karma has been disabled. karma is to test brownser modules.
   grunt.registerTask('build_and_watch', ['watch']);
   grunt.registerTask('doc', ['clean', 'jsdoc']);
+  grunt.registerTask('test', ['mochaTest', 'karma']);
+
 };
 
