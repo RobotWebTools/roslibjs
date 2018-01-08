e604d3a6e13194dc17e38dbc627951363b380179
Jihoon Lee <jihoonlee.in@gmail.com>
2018-01-08 15:55:23 +0900

    set user email

--- test/build_push.sh
+++ test/build_push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
+  git config --global user.email "jihoonlee.in@gmail.com"
   git config --global user.name "Travis CI"
 }
 
