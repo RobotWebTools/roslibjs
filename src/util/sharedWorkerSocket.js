be3ed5de68c63c2222252d9aa27d54950de77015
Adrien BARRAL <aba@robopec.com>
2022-01-27 18:05:49 +0100

    Add some explainations.

--- src/util/sharedWorkerSocket.js
+++ src/util/sharedWorkerSocket.js
@@ -8,11 +8,20 @@ const webworkify = require("webworkify");
 function SharedWorkerConnection(url) {
     var blob = webworkify(require("./sharedWorkerSocketImpl"), { bare: true });
     var workerUrl = URL.createObjectURL(blob);
+    // Here we would like to write : this.worker_ = new SharedWorker(workerUrl);
+    // But webworkify generate different URL at each call, so the worker is not shared
+    // beacause all URL are different.
+    // So we try the following trick :
     fetch(workerUrl).then(function (response) {
         response.text().then(function (workerSource) {
+            // here workerSource is javascript source code of the worker
             const workerURL = 'data:application/javascript;base64,' + btoa(workerSource);
+            // we juste create an url with the source code encoded in base64
             this.worker_ = new SharedWorker(workerURL);
-
+            // And we create a worker with this url. We can't use BroadcastChannel in this worker
+            // because of the "opaque origin" of such URL.
+            // But this still not work because webworkify add an UUID in the source code, and so the
+            // URL is always different...
             this.worker_.port.start();
             this.worker_.port.postMessage({ type: 'CONNECT', uri: url });
             const broadcastChannel = new BroadcastChannel('roslib.SharedWebSocketChannel');
