8c5751c5a38729dde9f14924f7469c3ef144dcc1
Adrien BARRAL <aba@robopec.com>
2022-02-01 18:54:53 +0100

    We receive a topic on two tabs with only one WebSocket Connection !!

--- src/util/sharedWorkerSocket.js
+++ src/util/sharedWorkerSocket.js
@@ -4,18 +4,40 @@
  * @param {string} url to connect to, e.g. "ws://localhost:9090".
  */
 function SharedWorkerConnection(url, sharedWorkerURL) {
-   console.log('Creating shared worker');
-   this.worker_ = new SharedWorker(sharedWorkerURL);
-   this.worker_.port.start();
-   this.worker_.port.postMessage({ type: 'CONNECT', uri: url });
-   this.worker_.port.onmessage = (ev) => {
-       console.log(ev.data);
-   };
+    console.log('Creating shared worker');
+    this.worker_ = new SharedWorker(sharedWorkerURL);
+    this.worker_.port.start();
+    this.worker_.port.postMessage({ type: 'CONNECT', uri: url });
+    this.worker_.port.onmessage = (ev) => {
+        this.handleWorkerMessage_(ev);
+    };
 }
 
-SharedWorkerConnection.prototype.close = function() {
+SharedWorkerConnection.prototype.send = function (data) {
+    this.worker_.port.postMessage({ type: 'WRITE', dataToWrite: data });
+}
+
+SharedWorkerConnection.prototype.handleWorkerMessage_ = function (ev) {
+    var data = ev.data;
+    if (data instanceof ArrayBuffer || typeof data === 'string') {
+        // binary or JSON message from rosbridge
+        this.onmessage(ev);
+    } else {
+        // control message from the wrapped WebSocket
+        var type = data.type;
+        if (type === 'close') {
+            this.onclose(null);
+        } else if (type === 'open') {
+            this.onopen(null);
+        } else if (type === 'error') {
+            this.onerror(null);
+        } else {
+            throw 'Unknown message from workersocket';
+        }
+    }
+};
+SharedWorkerConnection.prototype.close = function () {
     this.worker_.port.postMessage({ type: 'CLOSE' });
-    console.log('Closing One shared Worker');
 }
 
 module.exports = SharedWorkerConnection;
--- src/util/sharedWorkerSocketImpl.js
+++ src/util/sharedWorkerSocketImpl.js
@@ -1,7 +1,25 @@
-console.log('Attaching Shared Worker : ');
-//const broadcastChannel = new BroadcastChannel('roslib.SharedWebSocketChannel');
 var websocket = undefined;
 var allPorts = [];
+
+function handleSocketMessage(ev) {
+    console.log('Receiving message from WS : ' + ev.data);
+    var data = ev.data;
+
+    if (data instanceof ArrayBuffer) {
+        // binary message, transfer for speed
+        allPorts.forEach((port) => {
+            port.postMessage(data, [data]);
+        });
+    } else {
+        // JSON message, copy string
+        broadcastToAllPorts(data);
+    }
+}
+
+function handleSocketControl(ev) {
+    broadcastToAllPorts({ type: ev.type });
+}
+
 /**
  * @param {MessageEvent} messageEvent
  */
@@ -12,8 +30,13 @@ function onMessageFromMainThread(messageEvent) {
             if (websocket === undefined) {
                 console.log('Trying to connect to WS @ ' + messageEvent.data.uri);
                 websocket = new WebSocket(messageEvent.data.uri);
+                websocket.onmessage = handleSocketMessage;
+                websocket.onclose = handleSocketControl;
+                websocket.onopen = handleSocketControl;
+                websocket.onerror = handleSocketControl;
             } else {
                 console.log('Already connected to WS @ ' + messageEvent.data.uri);
+                messageEvent.currentTarget.postMessage({ type: 'open' });
             }
             break;
         case 'WRITE':
@@ -22,10 +45,17 @@ function onMessageFromMainThread(messageEvent) {
             }
             break;
         case 'CLOSE':
-            const portToRemove = messageEvent.data.port;
+            const portToRemove = messageEvent.currentTarget;
             const index = allPorts.indexOf(portToRemove);
-            if(index > -1) {
+            if (index > -1) {
                 allPorts.splice(index, 1);
+            }
+            // Paranoïd check : 
+            // If allPorts became empty the browser will shutdown
+            // this worker.
+            if (allPorts.length === 0) {
+                websocket.close();
+                websocket = undefined;
             }
             break;
     }
@@ -33,16 +63,16 @@ function onMessageFromMainThread(messageEvent) {
 /**
  * @param {MessageEvent} messageEvent
  */
-onconnect = function(messageEvent) {
+onconnect = function (messageEvent) {
     console.log('Some one is connected to worker ' + messageEvent);
-    var port = messageEvent.ports[0];    
+    var port = messageEvent.ports[0];
     allPorts.push(port);
     port.onmessage = onMessageFromMainThread;
-    
+
 }
 
-setInterval(() => {
+function broadcastToAllPorts(msg) {
     allPorts.forEach((port) => {
-        port.postMessage('toto ' + allPorts.length);
+        port.postMessage(msg);
     });
-}, 1000);
+}
