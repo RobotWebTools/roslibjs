5e8ac6df7e61b557ea4ca5f7582b3a9bec44f2c2
snorresovold <snorresovold@gmail.com>
2023-01-04 12:19:11 +0100

    Made a simple react example

---
+++ examples/react-example/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Send_message from "./component_examples/example_functions"
+
+function App() {
+  return (
+    <div className="App">
+      <Send_message/>
+    </div>
+  );
+}
+
+export default App;
