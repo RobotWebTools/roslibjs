d024c6cff96f880ab493c8e105c821eabfa77c9a
Ezra Brooks <ezra@brooks.cx>
2024-01-25 13:52:11 -0700

    Migrate from JSHint to ESLint

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const globals = require("globals");
+
+module.exports = [
+  {
+    languageOptions: {
+      "globals": {
+        ...globals.browser,
+        ...globals.node,
+        "bson": true
+      },
+    }
+  },
+  {
+    ignores: ['build/**/*.js', 'tsbuild/**/*.js']
+  },
+  {
+    rules: {
+      curly: 2,
+      eqeqeq: 2,
+      'wrap-iife': [2, 'any'],
+      'no-use-before-define': 0,
+      'new-cap': 2,
+      'no-caller': 2,
+      'dot-notation': 0,
+      'no-undef': 2,
+      'no-cond-assign': 0,
+      'no-eq-null': 0,
+      strict: 0,
+      quotes: [2, 'single'],
+      'no-proto': 2,
+      'linebreak-style': 2
+    },
+    files: ['Gruntfile.js', 'src/**/*.js']
+  }
+];
