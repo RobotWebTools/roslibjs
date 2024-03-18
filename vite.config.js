0c7bc1958e841e54f86dae9c2be424d49f113a0c
Ezra Brooks <ezra@brooks.cx>
2024-03-18 10:57:23 -0600

    Fix test file include pattern

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
   },
   test: {
     include: [
-      'src/**\/*.{test,spec}.?(c|m)[jt]s?(x)',
+      '{src,test}/**\/*.{test,spec}.?(c|m)[jt]s?(x)',
       './test/examples/*.js',
     ],
     exclude: ['dist'],
