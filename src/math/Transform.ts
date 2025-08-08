547b69aa189e830f769527c2c3badffbcbeac2dd
Drew Hoener <drewhoener@gmail.com>
2025-08-08 01:31:42 -0400

    refactor(Transform): Refactor Transform to typescript module, add interface to represent structure and type methods.
    Signed-off-by: Drew Hoener <drewhoener@gmail.com>

--- src/math/Transform.ts
+++ src/math/Transform.ts
@@ -3,29 +3,40 @@
  * @author David Gossow - dgossow@willowgarage.com
  */
 
-import Vector3 from './Vector3.js';
-import Quaternion from './Quaternion.js';
+import Vector3, { type IVector3 } from './Vector3.js';
+import Quaternion, { type IQuaternion } from './Quaternion.js';
+
+export interface ITransform {
+  /**
+   * The ROSLIB.Vector3 describing the translation.
+   */
+  translation: IVector3;
+  /**
+   * The ROSLIB.Quaternion describing the rotation.
+   */
+  rotation: IQuaternion;
+}
 
 /**
  * A Transform in 3-space. Values are copied into this object.
  */
-export default class Transform {
-  /**
-   * @param {Object} options
-   * @param {Vector3} options.translation - The ROSLIB.Vector3 describing the translation.
-   * @param {Quaternion} options.rotation - The ROSLIB.Quaternion describing the rotation.
-   */
-  constructor(options) {
+export default class Transform implements ITransform {
+
+  translation: Vector3;
+  rotation: Quaternion;
+
+  constructor(options: ITransform) {
     // Copy the values into this object if they exist
     this.translation = new Vector3(options.translation);
     this.rotation = new Quaternion(options.rotation);
   }
+
   /**
    * Clone a copy of this transform.
    *
    * @returns {Transform} The cloned transform.
    */
-  clone() {
+  clone(): Transform {
     return new Transform(this);
   }
 }
