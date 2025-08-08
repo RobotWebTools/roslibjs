52857525213a664148eb385190805ba8a5d6f684
Drew Hoener <drewhoener@gmail.com>
2025-08-08 01:18:19 -0400

    refactor(Vector3): Refactor Vector3 to typescript module, add interface to represent structure and type methods.
    Signed-off-by: Drew Hoener <drewhoener@gmail.com>

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,4 +1,4 @@
 export { default as Pose } from './Pose.js';
 export { default as Quaternion } from './Quaternion.js';
 export { default as Transform } from './Transform.js';
-export { default as Vector3 } from './Vector3.js';
+export { default as Vector3, type IVector3 } from './Vector3.js';
