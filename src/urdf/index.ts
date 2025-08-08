71a85c57fce36a9992cb5843d2fae0faf0e7da4b
Drew Hoener <drewhoener@gmail.com>
2025-08-08 02:16:57 -0400

    refactor(UrdfModel): Convert UrdfModel to TypeScript, replace loops with for-of, and improve type safety
    Signed-off-by: Drew Hoener <drewhoener@gmail.com>

--- src/urdf/index.ts
+++ src/urdf/index.ts
@@ -4,7 +4,7 @@ export { default as UrdfCylinder } from './UrdfCylinder.js';
 export { default as UrdfLink } from './UrdfLink.js';
 export { default as UrdfMaterial } from './UrdfMaterial.js';
 export { default as UrdfMesh } from './UrdfMesh.js';
-export { default as UrdfModel } from './UrdfModel.js';
+export { default as UrdfModel, type UrdfModelOptions } from './UrdfModel.js';
 export { default as UrdfSphere } from './UrdfSphere.js';
 export { default as UrdfVisual, type UrdfGeometryLike } from './UrdfVisual.js';
 
