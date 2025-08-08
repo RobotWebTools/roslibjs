55b135a185ca542961a12a6d0f335d29525b80f9
Drew Hoener <drewhoener@gmail.com>
2025-08-08 01:53:36 -0400

    refactor(UrdfCylinder): Convert UrdfCylinder to TypeScript, fix parsing warning.
    Signed-off-by: Drew Hoener <drewhoener@gmail.com>

--- src/urdf/UrdfCylinder.ts
+++ src/urdf/UrdfCylinder.ts
@@ -4,21 +4,21 @@
  * @author Russell Toris - rctoris@wpi.edu
  */
 
-import {UrdfType} from './UrdfTypes.js';
+import { type UrdfDefaultOptions, UrdfType, UrdfAttrs } from './UrdfTypes.js';
 
 /**
  * A Cylinder element in a URDF.
  */
 export default class UrdfCylinder {
-  /**
-   * @param {Object} options
-   * @param {Element} options.xml - The XML element to parse.
-   */
-  constructor(options) {
+
+  type: UrdfType;
+  length: number;
+  radius: number;
+
+  constructor({ xml }: UrdfDefaultOptions) {
     this.type = UrdfType.CYLINDER;
-    // @ts-expect-error -- possibly null
-    this.length = parseFloat(options.xml.getAttribute('length'));
-    // @ts-expect-error -- possibly null
-    this.radius = parseFloat(options.xml.getAttribute('radius'));
+
+    this.length = parseFloat(xml.getAttribute(UrdfAttrs.Length) ?? 'NaN');
+    this.radius = parseFloat(xml.getAttribute(UrdfAttrs.Radius) ?? 'NaN');
   }
 }
