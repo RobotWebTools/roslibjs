8fa9c1937fb2b6efc4e6477061806f17fc90eeb8
Ezra Brooks <ezra@brooks.cx>
2025-11-03 09:11:29 -0700

    Fix CI to use locked dependencies
    `npm ci` installs from package-lock instead of package,
    so it's idempotent and also faster.

--- test/build.bash
+++ test/build.bash
@@ -23,7 +23,7 @@ bash examples/setup_examples.bash
 echo -e "\e[1m\e[35mrostopic list\e[0m"
 rostopic list
 echo -e "\e[1m\e[35mnpm install\e[0m"
-npm install
+npm ci
 echo -e "\e[1m\e[35mnpm run build\e[0m"
 npm run build
 echo -e "\e[1m\e[35mnpm test\e[0m"
