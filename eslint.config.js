6c124c39c855af93fb142d62744222c41deeca4c
Ezra Brooks <ezra@brooks.cx>
2025-11-06 10:07:46 -0700

    Replace style rules in ESLint config with Prettier
    Prettier has basically become the de facto formatter for web tooling.
    Let's adopt it instead of homebrewing our own code style.

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 import jsdoc from 'eslint-plugin-jsdoc';
+import prettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -20,27 +21,22 @@ export default tseslint.config(
         projectService: true,
       },
     },
+    plugins: {
+      prettier
+    },
     rules: {
-      curly: 2,
       eqeqeq: 2,
       'wrap-iife': [2, 'any'],
       'no-use-before-define': 0,
       'no-caller': 2,
-      'dot-notation': 0,
       'no-undef': 2,
       'no-cond-assign': 0,
       'no-eq-null': 0,
       strict: 0,
-      quotes: [2, 'single'],
+      'prettier/prettier': 2,
       'no-proto': 2,
-      'linebreak-style': 2,
-      'key-spacing': [2, { afterColon: true }],
-      'eol-last': ['error', 'always'],
       // Disabled to allow namespaced ROS message types since that's how we think about message types in ROS
       '@typescript-eslint/no-namespace': 0,
-      indent: [2, 2],
-      'multiline-comment-style': 2,
-      'no-trailing-spaces': 2,
       // Plenty of APIs (like mocking APIs in Vitest) require empty functions to be declared.
       '@typescript-eslint/no-empty-function': 0
     },
