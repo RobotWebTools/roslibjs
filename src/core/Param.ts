813c563cf21914be2924b7b0d542334d88a9bf4e
Ezra Brooks <ezra@brooks.cx>
2025-11-06 18:06:09 -0700

    Port Param to TypeScript (#1012)

--- src/core/Param.ts
+++ src/core/Param.ts
@@ -3,39 +3,36 @@
  * @author Brandon Alexander - baalexander@gmail.com
  */
 
+import { rosapi } from "../types/rosapi.js";
+import Ros from "./Ros.js";
 import Service from "./Service.js";
 
 /**
  * A ROS parameter.
  */
-export default class Param {
-  ros;
-  name;
+export default class Param<T = unknown> {
+  ros: Ros;
+  name: string;
   /**
-   * @param {Object} options
-   * @param {import('../core/Ros.js').default} options.ros - The ROSLIB.Ros connection handle.
-   * @param {string} options.name - The param name, like max_vel_x.
+   * @param options
+   * @param options.ros - The ROSLIB.Ros connection handle.
+   * @param options.name - The param name, like max_vel_x.
    */
-  constructor({ ros, name }) {
+  constructor({ ros, name }: { ros: Ros; name: string }) {
     this.ros = ros;
     this.name = name;
   }
   /**
-   * @callback getCallback
-   * @param {Object} value - The value of the param from ROS.
-   */
-  /**
-   * @callback getFailedCallback
-   * @param {string} error - The error message reported by ROS.
-   */
-  /**
    * Fetch the value of the param.
    *
-   * @param {getCallback} callback - The callback function.
-   * @param {getFailedCallback} [failedCallback] - The callback function when the service call failed or the parameter retrieval was unsuccessful.
+   * @param callback - The callback function.
+   * @param [failedCallback] - The callback function when the service call failed or the parameter retrieval was unsuccessful.
    */
-  get(callback, failedCallback) {
-    const paramClient = new Service({
+  get(callback: (value: T) => void, failedCallback?: (error: string) => void) {
+    const paramClient = new Service<
+      rosapi.GetParamRequest,
+      rosapi.GetParamResponse
+    >({
       ros: this.ros,
       name: "rosapi/get_param",
       serviceType: "rosapi/GetParam",
@@ -57,22 +54,21 @@ export default class Param {
     );
   }
   /**
-   * @callback setParamCallback
-   * @param {Object} response - The response from the service request.
-   */
-  /**
-   * @callback setParamFailedCallback
-   * @param {string} error - The error message reported by ROS.
-   */
-  /**
    * Set the value of the param in ROS.
    *
-   * @param {Object} value - The value to set param to.
-   * @param {setParamCallback} [callback] - The callback function.
-   * @param {setParamFailedCallback} [failedCallback] - The callback function when the service call failed or the parameter setting was unsuccessful.
+   * @param value - The value to set param to.
+   * @param [callback] - The callback function.
+   * @param [failedCallback] - The callback function when the service call failed or the parameter setting was unsuccessful.
    */
-  set(value, callback, failedCallback) {
-    const paramClient = new Service({
+  set(
+    value: object,
+    callback?: (message: rosapi.SetParamResponse) => void,
+    failedCallback?: (error: string) => void,
+  ) {
+    const paramClient = new Service<
+      rosapi.SetParamRequest,
+      rosapi.SetParamResponse
+    >({
       ros: this.ros,
       name: "rosapi/set_param",
       serviceType: "rosapi/SetParam",
@@ -98,11 +94,17 @@ export default class Param {
   /**
    * Delete this parameter on the ROS server.
    *
-   * @param {setParamCallback} callback - The callback function.
-   * @param {setParamFailedCallback} [failedCallback] - The callback function when the service call failed or the parameter deletion was unsuccessful.
+   * @param callback - The callback function.
+   * @param [failedCallback] - The callback function when the service call failed or the parameter deletion was unsuccessful.
    */
-  delete(callback, failedCallback) {
-    const paramClient = new Service({
+  delete(
+    callback: (message: rosapi.DeleteParamResponse) => void,
+    failedCallback?: (error: string) => void,
+  ) {
+    const paramClient = new Service<
+      rosapi.DeleteParamRequest,
+      rosapi.DeleteParamResponse
+    >({
       ros: this.ros,
       name: "rosapi/delete_param",
       serviceType: "rosapi/DeleteParam",
