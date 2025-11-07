b4f4536c7f070bc615f7c0af34a4a4e10841badd
Ezra Brooks <ezra@brooks.cx>
2025-11-06 17:47:55 -0700

    Port Param to TypeScript

--- src/types/rosapi.ts
+++ src/types/rosapi.ts
@@ -26,4 +26,36 @@ export namespace rosapi {
   export interface PublishersResponse {
     publishers: string[];
   }
+  export interface GetParamRequest {
+    name: string;
+    default?: string;
+  }
+  interface GetParamResponseFailed {
+    value: never;
+    successful: false;
+    reason: string;
+  }
+  interface GetParamResponseSuccess {
+    value: string;
+    successful: true;
+    reason: never;
+  }
+  export type GetParamResponse =
+    | GetParamResponseFailed
+    | GetParamResponseSuccess;
+  export interface SetParamRequest {
+    name: string;
+    value: string;
+  }
+  export interface SetParamResponse {
+    successful: boolean;
+    reason: string;
+  }
+  export interface DeleteParamRequest {
+    name: string;
+  }
+  export interface DeleteParamResponse {
+    successful: boolean;
+    reason: string;
+  }
 }
