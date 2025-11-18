733975def8cee1b0d62f590cb037223010609129
Ezra Brooks <ezra@brooks.cx>
2025-11-18 11:36:14 -0700

    Add `@xmldom/xmldom` back to bundle (#1099)
    Temporary measure to close #1094. In roslibjs/ros2djs/ros3djs 3.0,
    we should figure out a better solution for all three libraries.

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
         useFlatConfig: true,
       },
     }),
-    externalizeDeps(),
+    externalizeDeps({ except: ["@xmldom/xmldom"] }),
   ],
   build: {
     lib: {
