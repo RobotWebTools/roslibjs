58c55b900961a104a05ad6f1b5793dbf00e39dbb
rick <rick@rick.rick>
2025-12-16 10:30:33 +0100

    chore: update cancelGoal to handle optional id parameter

--- packages/roslib/src/core/Action.ts
+++ packages/roslib/src/core/Action.ts
@@ -124,19 +124,9 @@ export default class Action<
   /**
    * Cancels an action goal.
    *
-   * @param [id] - The ID of the action goal to cancel. If not set or falsey; cancels all goals for this action.
+   * @param id - The ID of the action goal to cancel.
    */
-  cancelGoal(id?: string) {
-    if (!id) {
-      this.ros.callOnConnection({
-        op: "call_service",
-        service: `${this.name}/_action/cancel_goal`,
-        args: {},
-      });
-
-      return;
-    }
-
+  cancelGoal(id: string) {
     this.ros.callOnConnection({
       op: "cancel_action_goal",
       id: id,
