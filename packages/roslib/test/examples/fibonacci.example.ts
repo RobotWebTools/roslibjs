7e6c4207b361c2862730e0ef114baa287aa421da
rick <rick@rick.rick>
2025-12-16 13:21:03 +0100

    feat: implement integration tests for fibonacci example and cancel all goals.

--- packages/roslib/test/examples/fibonacci.example.ts
+++ packages/roslib/test/examples/fibonacci.example.ts
@@ -1,65 +1,159 @@
 import { describe, it, expect } from "vitest";
 import * as ROSLIB from "../../src/RosLib.ts";
 
-// Noetic is the only version of ROS 1 we support, so we skip based on distro name
-// instead of adding extra plumbing for ROS_VERSION.
-describe.skipIf(process.env["ROS_DISTRO"] !== "noetic")(
-  "ROS 1 Fibonacci Example",
-  function () {
-    it(
-      "Fibonacci",
-      () =>
-        new Promise<void>((done) => {
-          const ros = new ROSLIB.Ros({
-            url: "ws://localhost:9090",
-          });
-          /*
-           * The ActionClient
-           * ----------------
-           */
+describe("Fibonacci Example", function () {
+  // Noetic is the only version of ROS 1 we support, so we skip based on distro name
+  // instead of adding extra plumbing for ROS_VERSION.
+  it.skipIf(process.env["ROS_DISTRO"] !== "noetic")(
+    "Fibonacci ROS 1",
+    () =>
+      new Promise<void>((done) => {
+        const ros = new ROSLIB.Ros({
+          url: "ws://localhost:9090",
+        });
+        /*
+         * The ActionClient
+         * ----------------
+         */
 
-          const fibonacciClient = new ROSLIB.ActionClient({
-            ros: ros,
-            serverName: "/fibonacci",
-            actionName: "actionlib_tutorials/FibonacciAction",
-          });
+        const fibonacciClient = new ROSLIB.ActionClient({
+          ros: ros,
+          serverName: "/fibonacci",
+          actionName: "actionlib_tutorials/FibonacciAction",
+        });
 
-          // Create a goal.
-          const goal = new ROSLIB.Goal({
-            actionClient: fibonacciClient,
-            goalMessage: {
-              order: 7,
-            },
-          });
+        // Create a goal.
+        const goal = new ROSLIB.Goal({
+          actionClient: fibonacciClient,
+          goalMessage: {
+            order: 7,
+          },
+        });
 
-          // Print out their output into the terminal.
-          const items = [
-            { sequence: [0, 1, 1] },
-            { sequence: [0, 1, 1, 2] },
-            { sequence: [0, 1, 1, 2, 3] },
-            { sequence: [0, 1, 1, 2, 3, 5] },
-            { sequence: [0, 1, 1, 2, 3, 5, 8] },
-            { sequence: [0, 1, 1, 2, 3, 5, 8, 13] },
-            { sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21] },
-          ];
-          goal.on("feedback", function (feedback) {
-            expect(feedback).to.eql(items.shift());
-          });
-          goal.on("result", function (result) {
-            expect(result).to.eql({ sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21] });
+        // Print out their output into the terminal.
+        const items = [
+          { sequence: [0, 1, 1] },
+          { sequence: [0, 1, 1, 2] },
+          { sequence: [0, 1, 1, 2, 3] },
+          { sequence: [0, 1, 1, 2, 3, 5] },
+          { sequence: [0, 1, 1, 2, 3, 5, 8] },
+          { sequence: [0, 1, 1, 2, 3, 5, 8, 13] },
+          { sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21] },
+        ];
+        goal.on("feedback", function (feedback) {
+          expect(feedback).to.eql(items.shift());
+        });
+        goal.on("result", function (result) {
+          expect(result).to.eql({ sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21] });
+          done();
+        });
+
+        /*
+         * Send the goal to the action server.
+         * The timeout is to allow rosbridge to properly subscribe all the
+         * Action topics - otherwise, the first feedback message might get lost
+         */
+        setTimeout(function () {
+          goal.send();
+        }, 100);
+      }),
+    8000,
+  );
+
+  it(
+    "Fibonacci ROS 2",
+    () =>
+      new Promise<void>((done) => {
+        const ros = new ROSLIB.Ros({
+          url: "ws://localhost:9090",
+        });
+        /*
+         * The ActionClient
+         * ----------------
+         */
+
+        const fibonacciAction = new ROSLIB.Action({
+          ros,
+          name: "/fibonacci",
+          actionType: "action_tutorials_interfaces/action/Fibonacci",
+        });
+
+        const goal = { order: 8 };
+
+        // Print out their output into the terminal.
+        const items = [
+          { sequence: [0, 1, 1] },
+          { sequence: [0, 1, 1, 2] },
+          { sequence: [0, 1, 1, 2, 3] },
+          { sequence: [0, 1, 1, 2, 3, 5] },
+          { sequence: [0, 1, 1, 2, 3, 5, 8] },
+          { sequence: [0, 1, 1, 2, 3, 5, 8, 13] },
+          { sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21] },
+        ];
+
+        /*
+         * Send the goal to the action server.
+         */
+        fibonacciAction.sendGoal(
+          goal,
+          (result) => {
+            expect(result).to.eql({
+              sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21],
+            });
             done();
-          });
+          },
+          (feedback) => {
+            expect(feedback).to.eql(items.shift());
+          },
+        );
+      }),
+    8000,
+  );
 
-          /*
-           * Send the goal to the action server.
-           * The timeout is to allow rosbridge to properly subscribe all the
-           * Action topics - otherwise, the first feedback message might get lost
-           */
-          setTimeout(function () {
-            goal.send();
-          }, 100);
-        }),
-      8000,
+  it("Fibonacci ROS 2, cancel all goals", async () => {
+    const ros = new ROSLIB.Ros();
+    await ros.connect("ws://localhost:9090");
+
+    let resultCalled = false;
+    let failedCalled = false;
+
+    /*
+     * The Action
+     * ----------------
+     */
+    const fibonacciAction = new ROSLIB.Action({
+      ros,
+      name: "/fibonacci",
+      actionType: "action_tutorials_interfaces/action/Fibonacci",
+    });
+
+    const goal = { order: 8 };
+
+    /*
+     * Send the goal to the action server.
+     */
+    fibonacciAction.sendGoal(
+      goal,
+      // result callback.
+      () => {
+        resultCalled = true;
+      },
+      // feedback callback.
+      undefined,
+      // failed callback
+      () => {
+        failedCalled = true;
+      },
     );
-  },
-);
+
+    /*
+     * Cancel all goals.
+     */
+    fibonacciAction.cancelAllGoals();
+
+    setTimeout(() => {
+      expect(failedCalled).toBe(true);
+      expect(resultCalled).toBe(false);
+    }, 500); // wait 500ms to be sure the server handled the cancel request.
+  });
+});
